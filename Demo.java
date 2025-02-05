31909660eb145aeafc6e9fac83dc8a4fcf1f62b7
Venkey1st <venkateswarlug250@gmail.com>
2025-02-05 09:42:15 +0530

    Create Demo.java
    programm1

---
+++ Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		String a = "$5,000";
+		int b = Integer.parseInt(a);
+		System.out.print(b);/// numberformat expection
+
+	}
+
+}
